a035b5d3ae725d6676d591a06e3c23c521e9ed22
DevAniela <dismal.damsel@gmail.com>
2025-12-23 01:33:07 +0200

    feat: create monitor.rb and define service list

---
+++ monitor.rb
@@ -0,0 +1,9 @@
+puts "PulseCheck is starting..."
+
+services = [
+    "https://google.com",
+    "https://github.com",
+    "https://stripe.com"
+]
+
+puts "I will be monitoring: #{services.join(', ')}"
